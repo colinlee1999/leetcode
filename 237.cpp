94a85e00f7ca56e6ad1a806ddfd4fe6f12d2bbba
colinlee1999 <arteest@foxmail.com>
2016-06-25 21:15:26 +0800

    237 solved

---
+++ 237.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+#include "vector"
+#include "set"
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+		node->val = node->next->val;
+		node->next = node->next->next;
+    }
+};
+
+int main()
+{
+    Solution s;
+    cin.get();
+}
