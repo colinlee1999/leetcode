94f749bdbe97ff854193c72f6fc9abf322069905
colinlee1999 <arteest@foxmail.com>
2016-06-25 14:56:02 +0800

    344 solved

--- 344.cpp
+++ 344.cpp
@@ -1,6 +1,20 @@
 #include "iostream"
+using namespace std;
+
+class Solution {
+public:
+    string reverseString(string s) {
+        string s2 = "";
+        int l = s.length();
+        while (l-->0)
+            s2 += s[l];
+        return s2;
+    }
+};
 
 int main()
 {
-	return 0;
+    Solution s;
+    cout<<s.reverseString("abcd")<<endl;
+    cin.get();
 }
