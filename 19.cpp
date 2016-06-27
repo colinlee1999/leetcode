7a733b58950d0ce6ce3f144704eaa5f7a11524b4
colinlee1999 <arteest@foxmail.com>
2016-06-27 21:38:30 +0800

    19 solved

---
+++ 19.cpp
@@ -0,0 +1,47 @@
+#include "iostream"
+#include "vector"
+#include "map"
+#include "cmath"
+#include <stack>
+#include <queue>
+#include <sstream>
+#include <set>
+#include <unordered_map>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+	ListNode* removeNthFromEnd(ListNode* head, int n) {
+		ListNode *p, *q, *r;
+		p = head;
+		q = head;
+		while (n-- > 0) q = q->next;
+		while (q)
+		{
+			r = p;
+			p = p->next;
+			q = q->next;
+		}
+		if (p == head)
+		{
+			return head->next;
+		}
+		else
+		{
+			r->next = p->next;
+			return head;
+		}
+	}
+};
+
+int main()
+{
+	Solution s;
+	cin.get();
+}
