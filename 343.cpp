71c94d4b43a89170e0c98436926614218b3d4ae9
colinlee1999 <arteest@foxmail.com>
2016-07-20 23:24:40 +0800

    343 solved

---
+++ 343.cpp
@@ -0,0 +1,26 @@
+#include "iostream"
+#include "vector"
+#include "set"
+#include "map"
+using namespace std;
+
+class Solution {
+public:
+	int integerBreak(int n) {
+		if (n == 2) return 1;
+		if (n == 3) return 2;
+		int result = 1;
+		while (n > 4)
+		{
+			n -= 3;
+			result *= 3;
+		}
+		return result * n;
+	}
+};
+
+int main()
+{
+    Solution s;
+    cin.get();
+}
