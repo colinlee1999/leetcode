ed5bc19282f47ccc2a6140b28d26de92636975fa
colinlee1999 <arteest@foxmail.com>
2016-07-26 13:03:13 +0800

    94 solved, using two stacks to simulate dfs

---
+++ 94.cpp
@@ -0,0 +1,68 @@
+#include "iostream"
+#include "vector"
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	vector<int> inorderTraversal(TreeNode* root) {
+		stack<TreeNode *> s;
+		stack<int> instruction;
+		vector<int> result;
+		if (!root) return result;
+		s.push(root);
+		instruction.push(0);
+		while (!s.empty())
+		{
+			TreeNode * cur_node = s.top();
+			int cur_instruction = instruction.top();
+			switch (cur_instruction)
+			{
+			case 0:
+				instruction.top() = 1;
+				if (cur_node->left)
+				{
+					s.push(cur_node->left);
+					instruction.push(0);
+				}
+				break;
+			case 1:
+				result.push_back(cur_node->val);
+				instruction.top() = 2;
+				break;
+			case 2:
+				instruction.top() = 3;
+				if (cur_node->right)
+				{
+					s.push(cur_node->right);
+					instruction.push(0);
+				}
+				break;
+			case 3:
+				s.pop();
+				instruction.pop();
+				break;
+			default:
+				break;
+			}
+		}
+		return result;
+	}
+};
+
+int main()
+{
+	Solution s;
+    cin.get();
+}
