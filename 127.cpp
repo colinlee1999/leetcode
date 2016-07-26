a904597db2f9cab8bc27ad9daac1eeebc4b2c968
colinlee1999 <arteest@foxmail.com>
2016-07-26 22:41:06 +0800

    127 bfs improved, but still TLE

--- 127.cpp
+++ 127.cpp
@@ -13,78 +13,43 @@ using namespace std;
 class Solution {
 public:
 	int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
-		int wlen = wordList.size();
-		unordered_map<string, int> m;
-		vector<string> v;
-		vector<vector<int>> next(wlen, vector<int>());
-		vector<bool> b(wlen, false);
-		int count = 0;
-		for (auto it : wordList)
-		{
-			v.push_back(it);
-			m[it] = count++;
-		}			
-		if (m.find(beginWord) == m.end())
-		{
-			m[beginWord] = count++;
-			v.push_back(beginWord);
-			wlen++;
-		}
-		for (int i = 0; i < wlen; i++)
-			for (int j = i + 1; j < wlen; j++)
-				if (dist(v[i], v[j]) == 1)
-				{
-					next[i].push_back(j);
-					next[j].push_back(i);
-				}
 		int shortest_length = INT_MAX;
-		bfs(0, 
-			beginWord, 
-			endWord,
-			v,
-			next,
-			b,
-			m,
-			shortest_length);
+		int temp = dist(beginWord, endWord);
+		if (temp <= 1)
+			shortest_length = temp;
+		else
+		{
+			queue<pair<string, int>> q;
+			q.push(pair<string, int>(beginWord, 0));
+			wordList.erase(beginWord);
+			while (!q.empty())
+			{
+				pair<string, int> tfront = q.front();
+				q.pop();
+				unordered_set<string> to_be_removed;
+				bool found = false;
+				for (auto it : wordList)
+					if (dist(tfront.first, it) == 1)
+					{
+						q.push(pair<string, int>(it, tfront.second + 1));
+						int temp = dist(it, endWord);
+						if (temp <= 1)
+						{
+							shortest_length = tfront.second + 1 + temp;
+							found = true;
+							break;
+						}
+						to_be_removed.insert(it);
+					}
+				if (found) break;
+				for (auto it : to_be_removed)
+					wordList.erase(it);
+			}
+		}
 		return shortest_length == INT_MAX ? 0 : (shortest_length + 1);
 	}
 
 private:
-	void bfs(int depth,
-		string beginWord,
-		string endWord,
-		vector<string> & v,
-		vector<vector<int>> & next,
-		vector<bool> & b,
-		unordered_map<string, int> & m,
-		int & shortest_length)
-	{
-		queue<pair<string, int>> q;
-		q.push(pair<string, int>(beginWord, 0));
-		b[m[beginWord]] = true;
-		while (!q.empty())
-		{
-			pair<string, int> tfront = q.front();
-			int temp = dist(tfront.first, endWord);
-			if (temp <= 1)
-			{
-				shortest_length = tfront.second + temp;
-				return;
-			}
-			q.pop();
-			int cur_index = m[tfront.first];
-			for (int i = 0; i < next[cur_index].size(); i++)
-			{
-				int next_index = next[cur_index][i];
-				if (!b[next_index])
-				{
-					b[next_index] = true;
-					q.push(pair<string, int>(v[next_index], tfront.second + 1));
-				}
-			}
-		}			
-	}
-
 	int dist(string word1, string word2)
 	{
 		int d = 0;
