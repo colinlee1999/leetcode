c01e43d5435b6e2f2c326587e46d179d0c6af776
colinlee1999 <arteest@foxmail.com>
2016-08-12 22:16:33 -0500

    208 solved, trie tree implementation, reference http://www.geeksforgeeks.org/trie-insert-and-search/

---
+++ 208.cpp
@@ -0,0 +1,80 @@
+#include "iostream"
+#include "vector"
+#include "map"
+#include "cmath"
+#include <stack>
+using namespace std;
+
+#define CHAR_TO_INDEX(c) ((int)c - (int)'a')
+#define ALPHABET_SIZE (26)
+
+class TrieNode {
+public:
+	// Initialize your data structure here.
+	TrieNode() {
+		is_leaf = false;
+		for (int i = 0; i < ALPHABET_SIZE; i++)
+			children[i] = NULL;
+	}
+	TrieNode * children[ALPHABET_SIZE];
+	bool is_leaf;
+};
+
+class Trie {
+public:
+	Trie() {
+		root = new TrieNode();
+		root->is_leaf = true;
+	}
+
+	// Inserts a word into the trie.
+	void insert(string word) {
+		TrieNode * p = root;
+		for (auto it : word)
+		{
+			int index = CHAR_TO_INDEX(it);
+			if (!p->children[index])
+				p->children[index] = new TrieNode();
+			p = p->children[index];
+		}
+		p->is_leaf = true;
+	}
+
+	// Returns if the word is in the trie.
+	bool search(string word) {
+		TrieNode * p = root;
+		for (auto it : word)
+		{
+			int index = CHAR_TO_INDEX(it);
+			if (!p->children[index]) return false;
+			p = p->children[index];
+		}
+		return p->is_leaf;
+	}
+
+	// Returns if there is any word in the trie
+	// that starts with the given prefix.
+	bool startsWith(string prefix) {
+		TrieNode * p = root;
+		for (auto it : prefix)
+		{
+			int index = CHAR_TO_INDEX(it);
+			if (!p->children[index]) return false;
+			p = p->children[index];
+		}
+		return true;
+	}
+
+private:
+	TrieNode* root;
+};
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie;
+// trie.insert("somestring");
+// trie.search("key");
+
+int main()
+{
+    cin.get();
+}
