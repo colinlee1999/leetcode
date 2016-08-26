adf8aa18d2c1532cbfae7b2749a52698df00d3ac
colinlee1999 <arteest@foxmail.com>
2016-08-26 11:12:46 -0500

    367 solved, easy binary search

--- 337.cpp
+++ 337.cpp
@@ -7,43 +7,21 @@
 #include <random>
 using namespace std;
 
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
 class Solution {
 public:
-	int rob(TreeNode* root) {
-		if (!root) return 0;
-		int contain_root, not_contain_root;
-		return dfs(root, contain_root, not_contain_root);
-	}
-
-private:
-	int dfs(const TreeNode * root, int & contain_root, int & not_contain_root)
-	{
-		if (!root->left && !root->right)
+	bool isPerfectSquare(int num) {
+		if (num <= 1) return true;
+		int left, right, mid;
+		left = 1;
+		right = num;
+		while (left + 1 < right)
 		{
-			contain_root = root->val;
-			not_contain_root = 0;
+			mid = left + (right - left) / 2;
+			if (num / mid < mid) right = mid;
+			else left = mid;
 		}
-		else
-		{
-			int left_contain_root, left_not_contain_root;
-			int right_contain_root, right_not_contain_root;
-			int left_best, right_best;
-			left_contain_root = left_not_contain_root = 0;
-			right_contain_root = right_not_contain_root = 0;
-			left_best = right_best = 0;
-			if (root->left) left_best = dfs(root->left, left_contain_root, left_not_contain_root);
-			if (root->right) right_best = dfs(root->right, right_contain_root, right_not_contain_root);
-			contain_root = root->val + left_not_contain_root + right_not_contain_root;
-			not_contain_root = left_best + right_best;
-		}
-		return max(contain_root, not_contain_root);
+		if (left*left == num) return true;
+		else return false;
 	}
 };
 
