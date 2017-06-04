585e0c5f6e183bae7bf96ac5b571fe3b2766fef2
colinlee1999 <yfl@oak.work.simplemachinesinc.com>
2017-06-04 02:59:50 -0500

    587 solved, convex hull, but need all points on the edge

---
+++ 587.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <stack>
+#include <queue>
+#include <sstream>
+#include <set>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+struct Point {
+	int x;
+	int y;
+	Point() : x(0), y(0) {}
+	Point(int a, int b) : x(a), y(b) {}
+};
+
+Point pivot;
+
+int area2(Point a, Point b, Point c) {
+	return a.x * b.y - a.y * b.x + b.x * c.y - b.y * c.x + c.x * a.y - c.y * a.x;
+}
+
+int dist2(Point a, Point b) {
+	int dx = a.x - b.x;
+	int dy = a.y - b.y;
+	return dx * dx + dy * dy;
+}
+
+struct {
+	bool operator() (Point a, Point b) {
+		if (area2(pivot, a, b) == 0) {
+			return dist2(pivot, a) < dist2(pivot, b);
+		}
+		int d1x = a.x - pivot.x;
+		int d1y = a.y - pivot.y;
+		int d2x = b.x - pivot.x;
+		int d2y = b.y - pivot.y;
+		return (atan2((double)d1y, (double)d1x) - atan2((double)d2y, (double)d2x)) < 0;
+	}
+} angle_cmp;
+
+int turn(Point p, Point q, Point r) {
+	int result = (r.x - q.x) * (p.y - q.y) - (r.y - q.y) *(p.x - q.x);
+	if (result < 0) return -1;
+	if (result > 0) return 1;
+	return 0;
+}
+
+bool ccw(Point p, Point q, Point r) {
+	return turn(p, q, r) >= 0;
+}
+
+class Solution {
+public:
+	vector<Point> outerTrees(vector<Point>& points) {
+		int i, P0 = 0, N = points.size();
+		for (i = 1; i < N; i++) {
+			if (points[i].y < points[P0].y ||
+				(points[i].y == points[P0].y && points[i].x > points[P0].x)) {
+				P0 = i;
+			}
+		}
+		Point temp = points[0];
+		points[0] = points[P0];
+		points[P0] = temp;
+
+		pivot = points[0];
+		sort(++points.begin(), points.end(), angle_cmp);
+
+		stack<Point> S;
+		Point prev, now;
+		S.push(points[N - 1]);
+		S.push(points[0]);
+		
+		i = 1;
+		while (i < N) {
+			now = S.top();
+			S.pop();
+			prev = S.top();
+			S.push(now);
+			if (ccw(prev, now, points[i])) {
+				S.push(points[i]);
+				i++;
+			}
+			else {
+				S.pop();
+			}
+		}
+
+		vector<Point> ConvexHull;
+		while (!S.empty()) {
+			ConvexHull.push_back(S.top());
+			S.pop();
+		}
+
+		vector<Point> result;
+		for (i = 0; i < N; i++) {
+			for (int j = 0; j < ConvexHull.size() - 1; j++) {
+				if (turn(ConvexHull[j], ConvexHull[j + 1], points[i]) == 0) {
+					result.push_back(points[i]);
+					break;
+				}
+			}
+		}
+
+		return result;
+	}
+};
+
+int main()
+{
+	Solution s;
+	cin.get();
+}
