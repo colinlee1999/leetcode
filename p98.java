4e131e0ce3647942f64c89162519efea9ce6adcf
colinlee1999 <yfl@oak.work.simplemachinesinc.com>
2017-08-16 02:14:18 -0500

    98 solved, java 3 line solution, from top to bottom rather than the other way round, very ingenious

---
+++ p98.java
@@ -0,0 +1,25 @@
+import java.util.Objects;
+
+public class p98 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public class Solution {
+        public boolean isValidBST(TreeNode root) {
+            return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        }
+
+        private boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+            if (Objects.isNull(root)) return true;
+            if (root.val >= maxVal || root.val <= minVal) return false;
+            return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
+        }
+    }
+}
